4c09ac1a1a2de90d820db9598662578cf80ceffb
sakib <nsakib.cse@gmail.com>
2021-09-24 18:15:12 +0600

    Pass data to react component.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Products from "./products/products";
 
+const products = [
+    {
+        name: "Un Chien Andalou (film still)",
+        description: "One of the most acclaimed films in cinema history, Un Chien Andalou (An Andalusian Dog) (1929) is a short silent surrealist film by Spanish director Luis Buñuel and artist Salvador Dalí. Following the principles of Surrealism, the film does not have a narrative in the conventional sense.",
+        image: 'https://uploads7.wikiart.org/images/salvador-dali/un-chien-andalou-film-still-1928.jpg'
+    }
+]
 export default function Home() {
     return (
         <div className={styles.container}>
@@ -17,7 +24,7 @@ export default function Home() {
                     -- <a href="https://en.wikipedia.org/wiki/Salvador_Dal%C3%AD">Salvador Dalí!</a>
                 </h6>
 
-                <Products/>
+                <Products products={products}/>
             </main>
         </div>
     )
