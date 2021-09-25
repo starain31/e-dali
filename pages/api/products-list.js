6011740a529a00f4540a5d9de2f8201494630078
sakib <nsakib.cse@gmail.com>
2021-09-25 18:06:32 +0600

    Bugfix: in memory db issue in production

--- pages/api/products-list.js
+++ pages/api/products-list.js
@@ -1,9 +1,5 @@
-import {Low, JSONFile} from 'lowdb'
-
-const products = new Low(new JSONFile('db/products.json'));
+import {getProduct} from '../../db'
 
 export default async function handler(req, res) {
-    await products.read()
-
-    res.status(200).json({products: products.data});
+    res.status(200).json({products: getProduct()});
 }
--- pages/api/wish-list.js
+++ pages/api/wish-list.js
@@ -1,9 +1,5 @@
-import {Low, JSONFile} from 'lowdb'
-
-const wishList = new Low(new JSONFile('db/wishlist.json'));
+import {getWishList} from '../../db'
 
 export default async function handler(req, res) {
-    await wishList.read()
-
-    res.status(200).json({wishlist: wishList.data});
+    res.status(200).json({wishlist: getWishList()});
 }
