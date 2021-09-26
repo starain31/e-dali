d7c9e49f791b9f59dc7fb65776d813b30a836b68
sakib <nsakib.cse@gmail.com>
2021-09-27 01:15:28 +0600

    Add product-list endpoint error handle.

--- pages/api/products-list.js
+++ pages/api/products-list.js
@@ -1,5 +1,9 @@
 import {getProduct} from '../../service/products'
 
 export default async function handler(req, res) {
-    res.status(200).json({products: await getProduct({searchKey: req.query.searchKey})});
+    try {
+        res.status(200).json({products: await getProduct({searchKey: req.query.searchKey})});
+    } catch (e) {
+        res.status(400).json({message: "server error"});
+    }
 }
