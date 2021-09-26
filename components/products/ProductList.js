fc0087524b8d3aae7cfd0bfc0d5d9554473213ee
sakib <nsakib.cse@gmail.com>
2021-09-27 01:06:55 +0600

    Add Product search functionality.

--- components/products/ProductList.js
+++ components/products/ProductList.js
@@ -1,6 +1,8 @@
 import classes from './ProductList.module.css';
 import productClass from './Product.module.css';
 import Product from "./Product";
+import {useState} from "react";
+import Card from "../ui/Card";
 
 function ProductList({products}) {
     function adToWishList({id}) {
@@ -11,7 +13,7 @@ function ProductList({products}) {
             },
             body: JSON.stringify({id})
         }).then((r) => {
-            if(r.status === 0) {
+            if (r.status === 0) {
                 console.log(`Wishlist Successfully added.`)
             } else {
                 console.log(`Wishlist Adding failed.`)
@@ -21,23 +23,42 @@ function ProductList({products}) {
         });
     }
 
+    function handleSearch(e) {
+        fetch(`api/products-list?searchKey=${e.target.value}`,).then(r => r.json()).then(({products}) => {
+            setProductsState(products);
+        });
+    }
+
+    const [productsState, setProductsState] = useState(products);
+
     return (
-        <ul className={classes.list}>{
-            products.map((product) => (
-                <li key={product.id}>
-                    <Product
-                        name={product.name}
-                        description={product.description}
-                        image={product.image}
-                        ActionButton={
-                            <div className={productClass.actions}>
-                                <button onClick={() => adToWishList({id: product.id})}>ADD TO WISH LIST</button>
-                            </div>
-                        }
-                    />
-                </li>
-            ))
-        }</ul>
+        <div>
+            <Card>
+                <div className={classes.form}>
+                    <div className={classes.control}>
+                        <input type={`text`} onChange={handleSearch}/>
+                    </div>
+                </div>
+            </Card>
+
+
+            <ul className={classes.list}>{
+                productsState.map((product) => (
+                    <li key={product.id}>
+                        <Product
+                            name={product.name}
+                            description={product.description}
+                            image={product.image}
+                            ActionButton={
+                                <div className={productClass.actions}>
+                                    <button onClick={() => adToWishList({id: product.id})}>ADD TO WISH LIST</button>
+                                </div>
+                            }
+                        />
+                    </li>
+                ))
+            }</ul>
+        </div>
     );
 }
 
